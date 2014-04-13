ad607e9769fad9008ce5dd9ddb5f0e1bc24c1a89
Chevanlol360 <chevanlol360@CQ-NC696AA-ABA-HP.(none)>
2014-04-13 18:48:08 -0400

    Cpufreq: added hotplug,intellidemand,interactiveX Finally i get to add interactivex to my list of govs, might revert this if my kernel bugs out though. had to add "CONFIG_NO_ERROR_ON_MISMATCH=y" to my defconfig due to small compilation error. Gonna let this run and test it and if its all good i'll leave it as it is.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,15 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
+extern struct cpufreq_governor cpufreq_gov_hotplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 
